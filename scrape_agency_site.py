ba5acfed10babdf9c646e6bb971686df48ba56ac
Emmanuel Martinez <emmanuelmartinez@emmanuels-imac.att.net>
2016-03-30 21:46:24 -0700

    root url with each letter in alphabet

--- scrape_agency_site.py
+++ scrape_agency_site.py
@@ -1,4 +1,17 @@
+from string import ascii_lowercase
+
+
 class ScrapeAgencies:
     
     def __init__(self):
-        pass
+        
+        #create all urls with each letter in the alphabet
+        root_url = 'https://www.usa.gov/federal-agencies/'
+        for letter in ascii_lowercase:
+            print root_url + letter
+            
+            
+            
+if __name__ == "__main__":
+    scrape = ScrapeAgencies()
+    
