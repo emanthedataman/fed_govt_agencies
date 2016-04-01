0ed36e9aef7a0c0618556e132c11c3baaaf6697b
Emmanuel Martinez <emmanuelmartinez@emmanuels-imac.att.net>
2016-04-01 09:47:51 -0700

    print all urls with each letter

--- scrape_agency_site.py
+++ scrape_agency_site.py
@@ -1,24 +1,16 @@
 from string import ascii_lowercase as letters
 
-
 class ScrapeAgencies:
-    
-    def __init__(self, letter):
-        self.letter = letter
 
+    def __init__(self):
+        self.letter = letter  
         
-    #create all urls with each letter in the alphabet
     def create_urls(self, letter):
         root_url = 'https://www.usa.gov/federal-agencies/%s' % (self.letter)
-        return root_url
+        print root_url
         
-            
-            
-            
+        
 if __name__ == "__main__":
-    
     for letter in letters:
-    
-        scrape = ScrapeAgencies(letter)
-        
-    
+        scrape = ScrapeAgencies()
+        scrape.create_urls(letter)
