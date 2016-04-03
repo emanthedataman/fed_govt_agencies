6d10d849380818efe7cbd58cf9ae8f73ae152bf6
Emmanuel Martinez <emmanuelmartinez@emmanuels-imac.att.net>
2016-04-02 20:49:33 -0700

    print all index url through main function

--- scrape_agency_site.py
+++ scrape_agency_site.py
@@ -1,16 +1,21 @@
 from string import ascii_lowercase as letters
+import requests
 
 class ScrapeAgencies:
 
     def __init__(self):
         self.letter = letter  
         
-    def create_urls(self, letter):
-        root_url = 'https://www.usa.gov/federal-agencies/%s' % (self.letter)
-        print root_url
+        self.min_sleep_sec = 0.5
+        self.max_sleep_sec = 2.5
         
+    def create_urls(self, letter):
+        urls_list = []
+        root_url = 'https://www.usa.gov/federal-agencies/%s' % (self.letter)
+        return root_url
         
 if __name__ == "__main__":
     for letter in letters:
         scrape = ScrapeAgencies()
-        scrape.create_urls(letter)
+        index_urls = scrape.create_urls(letter)
+        print index_urls
